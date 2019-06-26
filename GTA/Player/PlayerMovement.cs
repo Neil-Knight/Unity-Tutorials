bcdb735ea5aec6842e0ba4dcb9ff6b56325804b8
AggrovatedSnail <51999585+AggrovatedSnail@users.noreply.github.com>
2019-06-26 15:58:37 +0100

    Update PlayerMovement.cs
    Updated code - cleaner version

--- GTA/Player/PlayerMovement.cs
+++ GTA/Player/PlayerMovement.cs
@@ -11,12 +11,15 @@ public class PlayerMovement : MonoBehaviour
     public float desiredRotationSpeed;
     public Animator animator;
     public Camera camera;
+    public bool isGrounded = false;
     public CharacterController controller;
     public float Angle2Target;
     public GameObject InputDirectionCompass;
     private float verticalVelocity;
     private Vector3 moveVector;
     private bool isInAir = false;
+    private bool isPistolArmed = false;
+    private bool isRifleArmed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -30,10 +33,10 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         InputMagnitude();
-        controller.Move(desiredMoveDirection);
     }
 
-    void InputMagnitude() {
+    void FixedUpdate()
+    {
         // Parent the Input Direction Compass to the Player
         InputDirectionCompass.transform.position = this.transform.position;
         InputDirectionCompass.transform.rotation = Quaternion.identity;
@@ -45,6 +48,28 @@ public class PlayerMovement : MonoBehaviour
         Speed = new Vector2(InputX, InputZ).sqrMagnitude;
         animator.SetFloat("InputMagnitude", Speed, 0f, Time.deltaTime);
 
+        Vector3 targetDirection = InputDirectionCompass.transform.InverseTransformPoint(desiredMoveDirection);
+        Angle2Target = Mathf.Atan2(targetDirection.x, targetDirection.z) * Mathf.Rad2Deg;
+
+        var cam = Camera.main;
+        var forward = cam.transform.forward;
+        var right = cam.transform.right;
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+        desiredMoveDirection = forward * InputZ + right * InputX;
+        desiredMoveDirection.y = 0f;
+
+        InputDirectionCompass.transform.Rotate(0, 0, Angle2Target);
+        InputDirectionCompass.transform.Translate(desiredMoveDirection);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), desiredRotationSpeed * Time.deltaTime);
+
+        controller.Move(desiredMoveDirection * Time.deltaTime);
+    }
+
+    void InputMagnitude() {
         bool running = Input.GetButton("Running");
         animator.SetBool("isRunning", running);
         if (running && Input.GetButtonDown("Sliding"))
@@ -58,6 +83,7 @@ public class PlayerMovement : MonoBehaviour
         bool punching = Input.GetButton("Fire1");
         animator.SetBool("isPunching", punching);
 
+        isGrounded = controller.isGrounded;
         animator.SetBool("isGrounded", controller.isGrounded);
 
         if (controller.isGrounded && !isInAir)
@@ -67,26 +93,26 @@ public class PlayerMovement : MonoBehaviour
             {
                 animator.SetBool("isJumping", jumping);
                 isInAir = true;
+                jumping = false;
             }
         }
         else
             isInAir &= !controller.isGrounded;
 
-        Vector3 targetDirection = InputDirectionCompass.transform.InverseTransformPoint(desiredMoveDirection);
-        Angle2Target = Mathf.Atan2(targetDirection.x, targetDirection.z) * Mathf.Rad2Deg;
+        if (Input.GetButton("ArmPistol"))
+        {
+            isPistolArmed = !isPistolArmed;
+            animator.SetBool("isPistolArmed", isPistolArmed);
+            isRifleArmed = false;
+            animator.SetBool("isRifleArmed", isRifleArmed);
+        }
 
-        InputDirectionCompass.transform.Rotate(0, 0, Angle2Target);
-        InputDirectionCompass.transform.Translate(desiredMoveDirection);
-
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), desiredRotationSpeed * Time.deltaTime);
-
-        var cam = Camera.main;
-        var forward = cam.transform.forward;
-        var right = cam.transform.right;
-        forward.y = 0;
-        right.y = 0;
-        forward.Normalize();
-        right.Normalize();
-        desiredMoveDirection = (forward * InputZ * Time.deltaTime) + (right * InputX * Time.deltaTime);
+        if (Input.GetButton("ArmRifle"))
+        {
+            isRifleArmed = !isRifleArmed;
+            animator.SetBool("isRifleArmed", isRifleArmed);
+            isPistolArmed = false;
+            animator.SetBool("isPistolArmed", isPistolArmed);
+        }
     }
 }
